767e67f55d0ec742c53bd640784823b0dfadde80
skyje <luca.tommasi.sc@gmail.com>
2020-11-12 13:40:44 +0100

    Update ServerThread.java

--- src/Server/ServerThread.java
+++ src/Server/ServerThread.java
@@ -1,4 +1,30 @@
 package Server;
+import java.net.*;
+import java.io.*;
 
-public class ServerThread {
+public class ServerThread extends Thread
+{
+private Socket socket;
+private String ClientIp;
+
+public ServerThread(Socket s,String ip) { socket=s;ClientIp=ip;  }
+
+public String getIp(){
+    return ClientIp;
+}
+
+public void run()
+{
+boolean fine=false;
+try
+{
+BufferedReader br=new BufferedReader(new
+InputStreamReader(socket.getInputStream()));
+PrintWriter pw=new PrintWriter(socket.getOutputStream());
+
+// IMPLEMENTAZIONE DEL PROTOCOLLO...    
+
+br.close(); pw.close(); socket.close();
+}catch(IOException e){System.out.println("Problema di rete");}
+}
 }
