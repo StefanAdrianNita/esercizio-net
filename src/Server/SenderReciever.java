e364e33c447a8cf3294783ea728ef99f6c6dc463
Daddie2 <50950554+Daddie2@users.noreply.github.com>
2020-11-12 19:17:10 +0100

    Update SenderReciever.java

--- src/Server/SenderReciever.java
+++ src/Server/SenderReciever.java
@@ -2,13 +2,17 @@ package Server;
 
 public class SenderReciever {
     //Attributi
-    String messaggioInAttesa;
-    String ipDestinatario;
-
+    private String messaggioInAttesa;
+    private String ipDestinatario;
+    private String ipMittente;
     //Metodi
     void riceviDaMittente(String msg){ //VIENE CHIAMATO DAL SERVERTHREAD MITTENTE
-
-    }
+    int num=msg.indexOf(" ");
+    ipMittente=msg.substring(0,num-1);
+    messaggioInAttesa=msg.substring(num+1,msg.length());
+    messaggioInAttesa=messaggioInAttesa.substring(0,messaggioInAttesa.indexOf(" ")-1);
+    ipMittente=msg.substring(messaggioInAttesa.length()+2, msg.length());
+}
 
     void inviaADestinatario(){ //VIENE CHIAMATO DA riceviDaMittente();
 
