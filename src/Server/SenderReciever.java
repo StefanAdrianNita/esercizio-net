45aab2e0576fd93085d5885385dfd48ae0bd9f29
Stefan Adrian Nita <stefanadrian.nita02@gmail.com>
2020-11-12 13:03:35 +0100

    Caricati placeholder

---
+++ src/Server/SenderReciever.java
@@ -0,0 +1,16 @@
+package Server;
+
+public class SenderReciever {
+    //Attributi
+    String messaggioInAttesa;
+    String ipDestinatario;
+
+    //Metodi
+    void riceviDaMittente(String msg){ //VIENE CHIAMATO DAL SERVERTHREAD MITTENTE
+
+    }
+
+    void inviaADestinatario(){ //VIENE CHIAMATO DA riceviDaMittente();
+
+    }
+}
