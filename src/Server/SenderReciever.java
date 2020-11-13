a3237ec8bf1a7289ce4dc16dee28376784ab93c9
Daddie2 <50950554+Daddie2@users.noreply.github.com>
2020-11-13 01:28:42 +0100

    Update SenderReciever.java

--- src/Server/SenderReciever.java
+++ src/Server/SenderReciever.java
@@ -6,15 +6,38 @@ public class SenderReciever {
     private String ipDestinatario;
     private String ipMittente;
     //Metodi
-    void riceviDaMittente(String msg){ //VIENE CHIAMATO DAL SERVERTHREAD MITTENTE
+    public void riceviDaMittente(String msg,String ip){ //VIENE CHIAMATO DAL SERVERTHREAD MITTENTE
     int num=msg.indexOf(" ");
-    ipMittente=msg.substring(0,num-1);
+    ipDestinatario=msg.substring(0,num-1);
     messaggioInAttesa=msg.substring(num+1,msg.length());
     messaggioInAttesa=messaggioInAttesa.substring(0,messaggioInAttesa.indexOf(" ")-1);
-    ipMittente=msg.substring(messaggioInAttesa.length()+2, msg.length());
+    ipMittente=ip;
+    inviaADestinatario();
 }
 
-    void inviaADestinatario(){ //VIENE CHIAMATO DA riceviDaMittente();
-
+    private void inviaADestinatario(){ //VIENE CHIAMATO DA riceviDaMittente();
+String ip=" ";
+int i=-1;
+while(ip!=null && ip!=ipDestinatario){
+i=i++;
+ip=Server.lista[i].Clientip;
+}
+if(ip==ipDestinatario){
+Server.lista[i].inviaMessaggio("RECIVE FROM "+ipMittente+" "+messaggioInAttesa);
+i=-1;
+while(ip!=ipMittente){
+    i=i++;
+    ip=Server.lista[i].Clientip;
     }
+    Server.lista[i].inviaMessaggio("MESSAGE SENT");
 }
+else{
+    i=-1;
+    while(ip!=ipMittente){
+        i=i++;
+        ip=Server.lista[i].Clientip;
+        }
+        Server.lista[i].inviaMessaggio("CONN_REFUSED");
+    }  
+}
+    }
